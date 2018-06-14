5c30f8e914899cc4b2f6526060fba8f1b032f40a
Wells <chawkeye@gmail.com>
2018-06-14 22:37:19 +0800

    設定後台路由

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :distances, only: [:index, :create, :show]
 
+  namespace :admin do
+    resources :services
+
+    root "services#index"
+  end
   root "distances#index"
 end
