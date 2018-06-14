4c3dc0a38749d1c6c866730d0502345fa2c448b5
Wells <chawkeye@gmail.com>
2018-06-14 22:36:32 +0800

    增加地理編碼ＡＰＩ

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -1,7 +1,16 @@
 class Distance < ApplicationRecord
   serialize :respond_list
 
-  def self.cal_distance(address)
+  def self.geocode(address)
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    key = 'AIzaSyCVtWcyzFy4AZBR7QMMdHUSG3maUcHieeo'
+    json_rep = RestClient.get url, {params: {address: address, language: 'zh-TW', key: key }}
+    respond = JSON.parse(json_rep)
+    if respond['status'] = 'OK'
+      return respond['results'][0]['formatted_address']
+    end
+  end
+  def cal_distance
     url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
     key = 'AIzaSyCVtWcyzFy4AZBR7QMMdHUSG3maUcHieeo'
     @destinations = Service.all
@@ -14,7 +23,7 @@ class Distance < ApplicationRecord
     end
     i = 0
     while dest.size > 0
-      json_rep = RestClient.get url, {params: {origins: address, destinations: dest.slice!(0,25).join("|") , language: 'zh-TW', key: key }}
+      json_rep = RestClient.get url, {params: {origins: self.geo_address, destinations: dest.slice!(0,25).join("|") , language: 'zh-TW', key: key }}
       respond = JSON.parse(json_rep)
       rep_array = respond['rows'][0]['elements']
       rep_array.each_with_index do |rep, index|
