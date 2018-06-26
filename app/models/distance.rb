e8bc6cd9d11b880ee2881b7fec616219cf74737a
Wells <chawkeye@gmail.com>
2018-06-26 20:57:54 +0800

    修改搜尋服務網條件

--- app/models/distance.rb
+++ app/models/distance.rb
@@ -3,7 +3,7 @@ class Distance < ApplicationRecord
 
   def self.geocode(address)
     url = 'https://maps.googleapis.com/maps/api/geocode/json'
-    key = 'AIzaSyCVtWcyzFy4AZBR7QMMdHUSG3maUcHieeo'
+    key = Rails.application.config_for(:map)['secret']
     json_rep = RestClient.get url, {params: {address: address, language: 'zh-TW', key: key }}
     respond = JSON.parse(json_rep)
     if respond['status'] = 'OK'
@@ -12,14 +12,18 @@ class Distance < ApplicationRecord
   end
   def cal_distance
     url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-    key = 'AIzaSyCVtWcyzFy4AZBR7QMMdHUSG3maUcHieeo'
+    key = Rails.application.config_for(:map)['secret']
     current_code = self.post_code.to_i
+    start_code = (( current_code / 10 ) - 1 ) * 10
+    end_code = ((( current_code / 10 ) + 1 ) * 10) + 9
+    puts start_code
+    puts end_code
     if current_code > 94
-      @destinations = Service.where(post_code: [(current_code - 10)..(current_code + 10)]).or(Service.where(post_code: [26..27]))
+      @destinations = Service.where(post_code: [start_code..end_code]).or(Service.where(post_code: [26..27]))
     elsif current_code > 25 and current_code < 28
-      @destinations = Service.where(post_code: [(current_code - 10)..(current_code + 10)]).or(Service.where(post_code: [95..98]))
+      @destinations = Service.where(post_code: [start_code..end_code]).or(Service.where(post_code: [95..98]))
     else
-      @destinations = Service.where(post_code: [(current_code - 10)..(current_code + 10)])
+      @destinations = Service.where(post_code: [start_code..end_code])
     end
     dest = @destinations.pluck(:lng, :lat).map { |t| t.join(",")}
 
