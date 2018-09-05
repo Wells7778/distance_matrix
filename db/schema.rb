bc36168a5e6b9b42dbcfcc8dae01adb87e46d468
Wells <chawkeye@gmail.com>
2018-09-05 20:35:21 +0800

    用devise 建立user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180615122450) do
+ActiveRecord::Schema.define(version: 20180905123453) do
 
   create_table "distances", force: :cascade do |t|
     t.string "address"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180615122450) do
     t.text "respond_list"
     t.text "geo_address"
     t.string "post_code"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "code"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "services", force: :cascade do |t|
@@ -30,6 +37,18 @@ ActiveRecord::Schema.define(version: 20180615122450) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "post_code"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
