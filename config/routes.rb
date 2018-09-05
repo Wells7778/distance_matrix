dc3b8cf2194e5cf9d49af6a52f35004c38a04861
Wells <chawkeye@gmail.com>
2018-09-05 20:47:44 +0800

    設定登入畫面

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :distances, only: [:index, :create, :show]
-
   namespace :admin do
     resources :services
-
     root "services#index"
   end
   root "distances#index"
