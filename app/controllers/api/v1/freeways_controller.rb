9831c34185b23b105e01c28fc7ef2d47f108c816
Wells <chawkeye@gmail.com>
2018-10-04 16:07:44 +0800

    完成Api controller

--- app/controllers/api/v1/freeways_controller.rb
+++ app/controllers/api/v1/freeways_controller.rb
@@ -1,2 +1,34 @@
 class Api::V1::FreewaysController < ApplicationController
+  def index
+    @freeways = Freeway.all
+    render json: {
+      data: @freeways.map do |freeway|
+        {
+          id: freeway.id,
+          name: freeway.name,
+          direction: freeway.direction
+        }
+      end
+    }
+  end
+
+  def show
+    @freeway = Freeway.find_by(id: params[:id])
+    @interchanges = @freeway.interchanges
+
+    render json: {
+      data: @interchanges.map do |i|
+        {
+          name: i.name,
+          cityNW: i.cityNW,
+          citySE: i.citySE,
+          km: i.km,
+          coordsNW: i.coordsNW,
+          coordsSE: i.coordsSE
+        }
+      end
+    }
+  end
+
+
 end
