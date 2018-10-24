3446221cb6376780987cea8d2dfdca8120ecdf2e
Wells <chawkeye@gmail.com>
2018-10-24 22:46:54 +0800

    result增加priority欄位

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181021132836) do
+ActiveRecord::Schema.define(version: 20181024141629) do
 
   create_table "freeways", force: :cascade do |t|
     t.string "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20181021132836) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.integer "priority", default: 0
   end
 
   create_table "services", force: :cascade do |t|
