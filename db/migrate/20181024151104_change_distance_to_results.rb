4bed22c1011e262ea835b588aed0cb954a108c3a
Wells <chawkeye@gmail.com>
2018-10-24 23:12:00 +0800

    fix migrate

---
+++ db/migrate/20181024151104_change_distance_to_results.rb
@@ -0,0 +1,6 @@
+class ChangeDistanceToResults < ActiveRecord::Migration[5.1]
+  def change
+    change_column :results, :distance, :integer
+    #Ex:- change_column("admin_users", "email", :string, :limit =>25)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024141629) do
+ActiveRecord::Schema.define(version: 20181024151104) do
 
   create_table "freeways", force: :cascade do |t|
     t.string "name"
