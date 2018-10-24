b12f377c26363b19695acceeb807e5e3a24a1396
Wells <chawkeye@gmail.com>
2018-10-24 22:56:16 +0800

    fix migrate

--- db/migrate/20181021130646_change_results.rb
+++ db/migrate/20181021130646_change_results.rb
@@ -2,6 +2,8 @@ class ChangeResults < ActiveRecord::Migration[5.1]
   def change
     rename_column :results, :responds, :distance
     add_column :results, :status, :string
+  end
+  def change
     change_column :results, :distance, :integer
   end
 end
