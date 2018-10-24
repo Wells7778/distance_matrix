3105c0661b4e47f4a7e6fc1edb3c1cae80af790d
Wells <chawkeye@gmail.com>
2018-10-24 23:01:27 +0800

    修migrate

--- db/migrate/20181021130646_change_results.rb
+++ db/migrate/20181021130646_change_results.rb
@@ -1,5 +1,6 @@
 class ChangeResults < ActiveRecord::Migration[5.1]
   def change
+    change_column :results, :responds, :integer
     rename_column :results, :responds, :distance
     add_column :results, :status, :string
   end
