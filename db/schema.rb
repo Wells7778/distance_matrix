b3bac29e4e669c4cd07dc67aeea74d8035bc66cf
Wells <chawkeye@gmail.com>
2018-10-31 21:40:34 +0800

    added model note

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181024152102) do
+ActiveRecord::Schema.define(version: 20181031134017) do
 
   create_table "freeways", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,12 @@ ActiveRecord::Schema.define(version: 20181024152102) do
     t.text "geo_address"
     t.string "post_code"
     t.string "latlng"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
