6743a0eb76b1b75ccf7f721d0e83834b1c66b722
Wells <chawkeye@gmail.com>
2018-11-04 22:26:15 +0800

    後台新增修改交流道功能

--- app/controllers/admin/interchanges_controller.rb
+++ app/controllers/admin/interchanges_controller.rb
@@ -1,14 +1,45 @@
 class Admin::InterchangesController < Admin::BaseController
-  before_action :set_freeway
+  before_action :set_freeway, only: :index
+  before_action :set_interchange, only: [:edit, :update, :destroy]
   def index
     @freeways = Freeway.all
     if params[:freeway_id]
-      @interchanges = @freeway.interchanges
-      @id = params[:freeway_id]
+      @interchanges = @freeway.interchanges.order(km: :asc)
     end
+    @interchange = Interchange.new
   end
 
+  def create
+    @interchange = Interchange.new(interchange_params)
+    @interchange.save
+    redirect_to admin_interchanges_path(freeway_id: interchange_params[:freeway_id])
+  end
+
+  def edit
+    @freeway = @interchange.freeway
+  end
+
+  def update
+    @interchange.update(interchange_params)
+    redirect_to admin_interchanges_path(freeway_id: @interchange.freeway.id)
+  end
+
+  def destroy
+    @interchange.destroy
+    redirect_to admin_interchanges_path(freeway_id: @interchange.freeway.id)
+  end
+
+
   private
+
+  def interchange_params
+    params.require(:interchange).permit(:name, :cityNW, :citySE, :km, :coordsNW, :coordsSE, :freeway_id)
+  end
+
+  def set_interchange
+    @interchange = Interchange.find(params[:id])
+  end
+
   def set_freeway
     @freeway = Freeway.find_by(id: params[:freeway_id])
   end
