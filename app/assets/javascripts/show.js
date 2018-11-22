87a028c8015da809d68d2bef82d6f2bd8fc99757
Wells <chawkeye@gmail.com>
2018-11-22 09:22:08 +0800

    服務網清單增加刪除線

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,17 @@
+$(document).on('turbolinks:load', function() {
+  if ($('#lists-show').length > 0) {
+    new Clipboard('.copy')
+    $('.copy').on('click', function(e) {
+      var tr = e.target.parentNode.parentNode
+      var serviceName = $(tr).find('.text-name')
+      $(serviceName).addClass('text-line-through bg-dark')
+    })
+    $('.disable-line-through').on('click', function(e) {
+      $('.text-line-through').removeClass('text-line-through bg-dark')
+    })
+  }
+})
+$(document).on('turbolinks:before-render', function() {
+  $('.disable-line-through').off('click', function() {})
+  $('.copy').off('click', function() {})
+})
